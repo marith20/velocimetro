9082d1e7546625a98d8ac3a768723bff569f5b4f
Maritza Milena Hernandez <marith20@gmail.com>
2016-01-25 08:33:50 -0500

    Velocimetro
    Semana 4 Tarea

---
+++ velocidades.playground/Contents.swift
@@ -0,0 +1,78 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+enum Velocidades : Int {
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init( velocidadInicial : Velocidades ) {
+        self = velocidadInicial
+    }
+}
+
+class Auto {
+    var velocidad : Velocidades
+    init() {
+        velocidad = Velocidades.init(velocidadInicial : Velocidades.Apagado)
+    }
+    
+    func cambioDeVelocidad() -> ( actual : Int, velocidadEnCadena : String ){
+        var actual : Int
+        var velocidadEnCadena : String
+        
+        actual = velocidad.rawValue
+        
+        switch velocidad {
+        case .Apagado:
+            velocidad = Velocidades.VelocidadBaja
+            velocidadEnCadena = "Apagado"
+        case .VelocidadBaja:
+            velocidad = Velocidades.VelocidadMedia
+            velocidadEnCadena = "Velocidad Baja"
+        case .VelocidadMedia:
+            velocidad = Velocidades.VelocidadAlta
+            velocidadEnCadena = "Velocidad Media"
+        case .VelocidadAlta:
+            velocidad = Velocidades.Apagado
+            velocidadEnCadena = "Velocidad Alta"
+        }
+        
+        return (actual, velocidadEnCadena)
+    }
+    
+    func cambioDeVelocidad2() -> ( actual : Int, velocidadEnCadena : String ){
+        var actual : Int
+        var velocidadEnCadena : String
+        
+        actual = velocidad.rawValue
+        
+        switch velocidad {
+        case .Apagado:
+            velocidad = Velocidades.VelocidadBaja
+            velocidadEnCadena = "Apagado"
+        case .VelocidadBaja:
+            velocidad = Velocidades.VelocidadMedia
+            velocidadEnCadena = "Velocidad Baja"
+        case .VelocidadMedia:
+            
+            velocidad = Velocidades.VelocidadAlta
+            velocidadEnCadena = "Velocidad Media"
+        case .VelocidadAlta:
+            velocidad = Velocidades.VelocidadMedia
+            velocidadEnCadena = "Velocidad Alta"
+        }
+        
+        return (actual, velocidadEnCadena)
+    }
+    
+}
+
+var miAuto = Auto()
+
+
+miAuto.velocidad = Velocidades.Apagado
+
+for var n = 1; n <= 20; n++ {
+    var tupla = miAuto.cambioDeVelocidad2()
+    print("\(n).\t\(tupla.actual), \(tupla.velocidadEnCadena)")
+}
